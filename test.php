9b6fd63eb387e92cf13885a8b73a871dd9e43eea
Yang Yu <purpleicecream520@gmail.com>
2016-10-01 09:43:34 +0800

    testphp

---
+++ test.php
@@ -0,0 +1,8 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+
+<?php
+    //phpinfo();
+	date_default_timezone_set('Asia/ShangHai');
+	echo "hello world!";
+	echo "<br/>现在时间：" . date("Y-m-d H:i:s");
+?>
